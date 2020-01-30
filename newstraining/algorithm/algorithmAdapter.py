50fad5d719e4e1f7d2a0b04b0cdcfb73a500510a
@girija2204 <gshbehera@gmail.com>
2020-01-30 08:17:02 +0530

    Update algorithmAdapter.py

--- newstraining/algorithm/algorithmAdapter.py
+++ newstraining/algorithm/algorithmAdapter.py
@@ -1,6 +1,7 @@
 from newsPortal.newsPortal.newstraining.preprocessor.contentPreprocessor import ContentPreprocessor
 from newsPortal.newsPortal.newstraining.algorithm.impl.neuralNetwork import NeuralNetwork
 
+
 class AlgorithmAdapter:
     class __AlgorithmAdapter:
         def __init__(self):
@@ -17,12 +18,11 @@ class AlgorithmAdapter:
     def __getattr__(self, item):
         return getattr(self.instance, item)
 
-    def initiateDetection(self,trainingInput):
+    def initiateDetection(self, trainingInput):
         preprocessor = ContentPreprocessor()
         preprocessedTrainingInput = preprocessor.preprocess(trainingInput)
         fndAlgo = self.getFNDAlgo()
         fndAlgo.train()
-
 
     # get the configuration for trainingAlgo to use
     def getFNDAlgo(self):
